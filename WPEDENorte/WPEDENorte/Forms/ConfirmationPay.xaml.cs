bca3f7f8696fb1ca9bad54e834bceb422fcb0e95
Mauricio <cvelez.1cero1@gmail.com>
2021-09-27 19:24:04 -0500

    vista metodos de pago
    vista metodos de pago

---
+++ WPEDENorte/WPEDENorte/Forms/ConfirmationPay.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPEDENorte.Forms
+{
+    /// <summary>
+    /// Interaction logic for ConfirmationPay.xaml
+    /// </summary>
+    public partial class ConfirmationPay : Window
+    {
+        public ConfirmationPay()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDelete_TouchDown(object sender, TouchEventArgs e)
+        {
+
+        }
+
+        private void BtnCash_TouchDown(object sender, TouchEventArgs e)
+        {
+
+        }
+
+        private void BtnCard_TouchDown(object sender, TouchEventArgs e)
+        {
+
+        }
+
+        private void BtnNo_TouchDown(object sender, TouchEventArgs e)
+        {
+
+        }
+    }
+}
