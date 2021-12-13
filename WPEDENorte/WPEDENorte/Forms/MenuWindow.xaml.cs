9a9f454e2bf96f1ddd9d463ea8586ba417a5f760
ecitySoftware <ecitySoftware2021@gmail.com>
2021-12-13 15:19:39 -0500

    Debug Conexion DB

--- WPEDENorte/WPEDENorte/Forms/MenuWindow.xaml.cs
+++ WPEDENorte/WPEDENorte/Forms/MenuWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using WPEDENorte.Classes;
+using WPEDENorte.LocalBD;
 
 namespace WPEDENorte.Forms
 {
@@ -72,6 +74,27 @@ namespace WPEDENorte.Forms
             }
             catch (Exception ex)
             {
+            }
+        }
+
+        private void btnConexionDB_TouchDown(object sender, TouchEventArgs e)
+        {
+            using (var conexion = new EDENorteBDEntities())
+            {
+                if (conexion.Database.Exists())
+                {
+                    btnConexionDB.Content = "Conexion BD exitosa.";
+
+                    string consultaFacturasCliente = "SELECT * FROM Tbl_Facturas WHERE Ref_Pago = '8142014144'";
+
+                    var registrosTabla = conexion.Tbl_Facturas.SqlQuery(consultaFacturasCliente).ToList().Count;
+
+                    ResultadoConexionDB.Content = "Resgistros: " + registrosTabla;
+                }
+                else
+                {
+                    btnConexionDB.Content = "Conexion BD fallida.";
+                }
             }
         }
     }
